34872594ccadc31c205a4789e1afa7d5fff4f392
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2022-09-29 14:49:15 +0100

    Removing log

--- gulp/typescript.js
+++ gulp/typescript.js
@@ -5,7 +5,6 @@ const config = require('./config.json')
 const ts = require("gulp-typescript");
 const tsProject = ts.createProject("./tsconfig.json");
 gulp.task("typescript", function () {
-    console.log(tsProject.length)
     return tsProject.src()
     .pipe(tsProject())
     .js.pipe(gulp.dest(config.paths.public + '/javascripts/'));
