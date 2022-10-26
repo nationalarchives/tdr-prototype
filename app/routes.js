e85b33118043b72000c8f3f9a83b214e49c759fa
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2022-10-26 15:02:16 +0100

    Main prototype updated to reflect latest changes to closure MD

--- app/routes.js
+++ app/routes.js
@@ -27,20 +27,53 @@ router.get(
 router.get(
   "/metadata/closure-metadata/baking-powder/confirm-closure-status",
   function (req, res) {
-    // file[] is open/undefined && data[] is open/undefined ( intention is to continue without confirmation - needs error warning )
-    if (req.session.data["cocao"] === undefined) {
-      // console.log("send to /check-closure-status?error=no-confirmation")
+    // file[] is open/undefined && data[] is open/undefined
+    // ( intention is to continue without confirmation - needs error warning )
+    if (req.session.data["closure"]["baking-powder"] === undefined) {
       req.session.data["error"] = "no-confirmation";
-      res.redirect("/metadata/closure-metadata/T2-flour/check-closure-status");
-    } else if (req.session.data["cocao"][0] === "true") {
+      res.redirect(
+        "/metadata/closure-metadata/baking-powder/check-closure-status"
+      );
+    } else if (req.session.data["closure"]["baking-powder"][0] === "true") {
       req.session.data["error"] = "";
-      res.redirect("/metadata/closure-metadata/T2-flour/add-closure");
-    } else if (req.session.data["cocao"] === "false") {
+      res.redirect("/metadata/closure-metadata/baking-powder/add-closure");
+    } else if (req.session.data["closure"]["baking-powder"] === "delete") {
       req.session.data["error"] = "";
+      delete req.session.data["is-the-title-sensitive"];
       res.redirect("/metadata/closure-metadata/file-level");
     } else {
-      // req.session.files['cocao'] = undefined;
-      res.redirect("/metadata/closure-metadata/T2-flour/huh");
+      res.redirect("/metadata/closure-metadata/baking-powder/huh");
+    }
+  }
+);
+
+router.get(
+  "/metadata/closure-metadata/baking-powder/add-closure-confirm/",
+  function (req, res) {
+    if (req.session.data["exemption-code-redir"] == "true") {
+      res.redirect("/metadata/closure-metadata/baking-powder/add-multiple-FOI");
+    } else if (
+      req.session.data["is-the-title-sensitive"] &&
+      req.session.data["is-the-description-sensitive"]
+    ) {
+      if (
+        req.session.data["is-the-title-sensitive"] == "yes" ||
+        req.session.data["is-the-description-sensitive"] == "yes"
+      ) {
+        req.session.data["error"] = "";
+        res.redirect(
+          "/metadata/closure-metadata/baking-powder/closure-alternative-title"
+        );
+      } else if (
+        req.session.data["is-the-title-sensitive"] == "no" &&
+        req.session.data["is-the-description-sensitive"] == "no"
+      ) {
+        req.session.data["error"] = "";
+        res.redirect("/metadata/closure-metadata/baking-powder/closure-added");
+      }
+    } else {
+      req.session.data["error"] = "no-alternative";
+      res.redirect("/metadata/closure-metadata/baking-powder/add-closure");
     }
   }
 );
@@ -50,45 +83,7 @@ router.get(
   function (req, res) {
     // console.log(req.session.data.foi_id_selection);
     // req.session.data.foi_id_selection = JSON.parse(req.params.ids);
-    res.redirect("/metadata/closure-metadata/T2-flour/add-closure");
-  }
-);
-
-router.get(
-  "/metadata/closure-metadata/T2-flour/confirm-multiple-FOI/",
-  function (req, res) {
-    // console.log(req.session.data.foi_id_selection);
-    // req.session.data.foi_id_selection = JSON.parse(req.params.ids);
-    res.redirect("/metadata/closure-metadata/T2-flour/add-closure");
-  }
-);
-
-// XHR update
-router.get(
-  "/metadata/closure-metadata/T2-flour/add-multiple-FOI/update/:ids",
-  function (req, res) {
-    req.session.data.foi_id_selection = JSON.parse(req.params.ids);
-    // To store sesssion data need an arbritrary redirect
-    res.redirect("/metadata/closure-metadata/T2-flour/add-multiple-FOI");
-  }
-);
-
-router.get(
-  "/metadata/closure-metadata/T2-flour/add-multiple-FOI/clear",
-  function (req, res) {
-    req.session.data.foi_id_selection = [];
-    res.redirect("/metadata/closure-metadata/T2-flour/add-multiple-FOI");
-  }
-);
-
-router.get(
-  "/metadata/closure-metadata/T2-flour/remove-FOI/:foiId",
-  function (req, res) {
-    req.session.data.foi_id_selection =
-      req.session.data.foi_id_selection.filter(
-        (code) => code !== req.params.foiId.toString()
-      );
-    res.redirect("/metadata/closure-metadata/T2-flour/add-closure");
+    res.redirect("/metadata/closure-metadata/baking-powder/add-closure");
   }
 );
 
