cdc99f95f618cf66d5b98a44f3ca343858530dfb
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2022-12-02 11:24:17 +0000

    Initial prototype code for multi-select filter

--- app/assets/javascripts/tdr-option-select.js
+++ app/assets/javascripts/tdr-option-select.js
@@ -1,5 +1,6 @@
 window.GOVUK = window.GOVUK || {};
 window.GOVUK.Modules = window.GOVUK.Modules || {};
+console.log(window.GOVUK);
 
 (function (Modules) {
   /* This JavaScript provides two functional enhancements to option-select components:
@@ -20,29 +21,19 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
     this.$allCheckboxes = this.$optionsContainer.querySelectorAll(
       ".govuk-checkboxes__item"
     );
-    this.hasFilter =
-      this.$optionSelect.getAttribute("data-filter-element") || "";
+
+    this.$filter = this.$optionSelect.querySelector(
+      'input[name="option-select-filter"]'
+    );
 
     this.checkedCheckboxes = [];
+    this.checkboxLabels = [];
+    this.showCheckboxes = [];
+    this.filterTimeout = 0;
   }
 
   OptionSelect.prototype.init = function () {
-    if (this.hasFilter.length) {
-      var filterEl = document.createElement("div");
-      filterEl.innerHTML = this.hasFilter;
-
-      var optionSelectFilter = document.createElement("div");
-      optionSelectFilter.classList.add("app-c-option-select__filter");
-      optionSelectFilter.innerHTML = filterEl.childNodes[0].nodeValue;
-
-      this.$optionsContainer.parentNode.insertBefore(
-        optionSelectFilter,
-        this.$optionsContainer
-      );
-
-      this.$filter = this.$optionSelect.querySelector(
-        'input[name="option-select-filter"]'
-      );
+    if (this.$filter) {
       this.$filterCount = document.getElementById(
         this.$filter.getAttribute("aria-describedby")
       );
@@ -52,10 +43,9 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
         " " + this.$filterCount.getAttribute("data-multiple");
       this.filterTextSelected =
         " " + this.$filterCount.getAttribute("data-selected");
-      this.checkboxLabels = [];
-      this.filterTimeout = 0;
 
       this.getAllCheckedCheckboxes();
+
       for (var i = 0; i < this.$allCheckboxes.length; i++) {
         this.checkboxLabels.push(
           this.cleanString(this.$allCheckboxes[i].textContent)
@@ -65,38 +55,20 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       this.$filter.addEventListener("keyup", this.typeFilterText.bind(this));
     }
 
+    this.setupHeight();
+    this.updateCheckedCount();
+    // this.updateFilteredCount();
+
     // Attach listener to update checked count
-    this.$optionsContainer
-      .querySelector(".gem-c-checkboxes__list")
-      .addEventListener("change", this.updateCheckedCount.bind(this));
-
-    // Replace div.container-head with a button
-    this.replaceHeadingSpanWithButton();
-
-    // Add js-collapsible class to parent for CSS
-    this.$optionSelect.classList.add("js-collapsible");
-
-    // Add open/close listeners
-    var button = this.$optionSelect.querySelector(".js-container-button");
-    button.addEventListener("click", this.toggleOptionSelect.bind(this));
-
-    var closedOnLoad = this.$optionSelect.getAttribute("data-closed-on-load");
-    if (closedOnLoad === "true") {
-      this.close();
-    } else {
-      this.setupHeight();
-    }
-
-    var checkedString = this.checkedString();
-    if (checkedString) {
-      this.attachCheckedCounter(checkedString);
-    }
+    this.$optionList.addEventListener(
+      "change",
+      this.updateCheckedCount.bind(this)
+    );
   };
 
   OptionSelect.prototype.typeFilterText = function (event) {
     event.stopPropagation();
     var ENTER_KEY = 13;
-
     if (event.keyCode !== ENTER_KEY) {
       clearTimeout(this.filterTimeout);
       this.filterTimeout = setTimeout(
@@ -106,7 +78,7 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
         300
       );
     } else {
-      event.preventDefault(); // prevents finder forms from being submitted when user presses ENTER
+      event.preventDefault(); // prevents forms from being submitted when user presses ENTER
     }
   };
 
@@ -129,11 +101,14 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
     };
 
   OptionSelect.prototype.doFilter = function doFilter(obj) {
+    console.log(this, obj);
     var filterBy = obj.cleanString(obj.$filter.value);
+    // Copy checked checkboxes so they are always displayed.
     var showCheckboxes = obj.checkedCheckboxes.slice();
-    var i = 0;
 
+    var i = 0;
     for (i = 0; i < obj.$allCheckboxes.length; i++) {
+      // If not checked or found by filter.
       if (
         showCheckboxes.indexOf(i) === -1 &&
         obj.checkboxLabels[i].search(filterBy) !== -1
@@ -142,131 +117,55 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
       }
     }
 
+    // Hide all
     for (i = 0; i < obj.$allCheckboxes.length; i++) {
       obj.$allCheckboxes[i].style.display = "none";
     }
 
+    // Show checked and found by filter
     for (i = 0; i < showCheckboxes.length; i++) {
       obj.$allCheckboxes[showCheckboxes[i]].style.display = "block";
     }
 
-    var lenChecked = obj.$optionsContainer.querySelectorAll(
-      ".govuk-checkboxes__input:checked"
-    ).length;
-    var len = showCheckboxes.length + lenChecked;
-    var html =
-      len +
-      (len === 1 ? obj.filterTextSingle : obj.filterTextMultiple) +
-      ", " +
-      lenChecked +
-      obj.filterTextSelected;
-    obj.$filterCount.innerHTML = html;
+    this.showCheckboxes = showCheckboxes;
+    obj.updateFilteredCount();
   };
 
-  OptionSelect.prototype.replaceHeadingSpanWithButton =
-    function replaceHeadingSpanWithButton() {
-      /* Replace the span within the heading with a button element. This is based on feedback from Léonie Watson.
-       * The button has all of the accessibility hooks that are used by screen readers and etc.
-       * We do this in the JavaScript because if the JavaScript is not active then the button shouldn't
-       * be there as there is no JS to handle the click event.
-       */
-      var containerHead = this.$optionSelect.querySelector(
-        ".js-container-button"
-      );
-      var jsContainerHeadHTML = containerHead.innerHTML;
+  OptionSelect.prototype.updateFilteredCount = function updateFilteredCount() {
+    var lenChecked =
+      this.$optionsContainer.querySelectorAll("input:checked").length;
+    var lenFound = this.showCheckboxes.length;
+    var html =
+      lenFound +
+      (lenFound === 1 ? this.filterTextSingle : this.filterTextMultiple) +
+      ", " +
+      lenChecked +
+      this.filterTextSelected;
 
-      // Create button and replace the preexisting html with the button.
-      var button = document.createElement("button");
-      button.setAttribute(
-        "class",
-        "js-container-button app-c-option-select__title app-c-option-select__button"
-      );
-      // Add type button to override default type submit when this component is used within a form
-      button.setAttribute("type", "button");
-      button.setAttribute("aria-expanded", true);
-      button.setAttribute("id", containerHead.getAttribute("id"));
-      button.setAttribute(
-        "aria-controls",
-        this.$optionsContainer.getAttribute("id")
-      );
-      button.innerHTML = jsContainerHeadHTML;
-      containerHead.parentNode.replaceChild(button, containerHead);
-    };
-
-  OptionSelect.prototype.attachCheckedCounter = function attachCheckedCounter(
-    checkedString
-  ) {
-    var element = document.createElement("div");
-    element.setAttribute(
-      "class",
-      "app-c-option-select__selected-counter js-selected-counter"
-    );
-    element.innerHTML = checkedString;
-    this.$optionSelect
-      .querySelector(".js-container-button")
-      .insertAdjacentElement("afterend", element);
+    this.$filterCount.innerHTML = html;
   };
 
   OptionSelect.prototype.updateCheckedCount = function updateCheckedCount() {
-    var checkedString = this.checkedString();
+    this.getAllCheckedCheckboxes();
+
+    var checkedString = this.getCheckedString();
     var checkedStringElement = this.$optionSelect.querySelector(
       ".js-selected-counter"
     );
 
     if (checkedString) {
-      if (checkedStringElement === null) {
-        this.attachCheckedCounter(checkedString);
-      } else {
+      if (checkedStringElement !== null) {
         checkedStringElement.textContent = checkedString;
       }
-    } else if (checkedStringElement) {
-      checkedStringElement.parentNode.removeChild(checkedStringElement);
     }
   };
 
-  OptionSelect.prototype.checkedString = function checkedString() {
-    this.getAllCheckedCheckboxes();
-    var count = this.checkedCheckboxes.length;
-    var checkedString = false;
-    if (count > 0) {
-      checkedString = count + " selected";
-    }
+  OptionSelect.prototype.getCheckedString = function getCheckedString() {
+    var checkedString = "";
+    var count = this.$optionsContainer.querySelectorAll("input:checked").length;
+    checkedString = count + " selected";
 
     return checkedString;
-  };
-
-  OptionSelect.prototype.toggleOptionSelect = function toggleOptionSelect(e) {
-    if (this.isClosed()) {
-      this.open();
-    } else {
-      this.close();
-    }
-    e.preventDefault();
-  };
-
-  OptionSelect.prototype.open = function open() {
-    if (this.isClosed()) {
-      this.$optionSelect
-        .querySelector(".js-container-button")
-        .setAttribute("aria-expanded", true);
-      this.$optionSelect.classList.remove("js-closed");
-      this.$optionSelect.classList.add("js-opened");
-      if (!this.$optionsContainer.style.height) {
-        this.setupHeight();
-      }
-    }
-  };
-
-  OptionSelect.prototype.close = function close() {
-    this.$optionSelect.classList.remove("js-opened");
-    this.$optionSelect.classList.add("js-closed");
-    this.$optionSelect
-      .querySelector(".js-container-button")
-      .setAttribute("aria-expanded", false);
-  };
-
-  OptionSelect.prototype.isClosed = function isClosed() {
-    return this.$optionSelect.classList.contains("js-closed");
   };
 
   OptionSelect.prototype.setContainerHeight = function setContainerHeight(
@@ -280,8 +179,9 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
   ) {
     var initialOptionContainerHeight = this.$optionsContainer.clientHeight;
     var optionListOffsetTop = this.$optionList.getBoundingClientRect().top;
+    // Using parentElement because the `input` is position:absolute.
     var distanceFromTopOfContainer =
-      option.getBoundingClientRect().top - optionListOffsetTop;
+      option.parentElement.getBoundingClientRect().top - optionListOffsetTop;
     return distanceFromTopOfContainer < initialOptionContainerHeight;
   };
 
@@ -293,11 +193,6 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
           visibleCheckboxes.push(this.$options[i]);
         }
       }
-
-      // add an extra checkbox, if the label of the first is too long it collapses onto itself
-      if (this.$options[visibleCheckboxes.length]) {
-        visibleCheckboxes.push(this.$options[visibleCheckboxes.length]);
-      }
       return visibleCheckboxes;
     };
 
@@ -305,6 +200,7 @@ window.GOVUK.Modules = window.GOVUK.Modules || {};
     var initialOptionContainerHeight = this.$optionsContainer.clientHeight;
     var height = this.$optionList.offsetHeight;
 
+    // console.log(initialOptionContainerHeight, height);
     // check whether this is hidden by progressive disclosure,
     // because height calculations won't work
     // would use offsetParent === null but for IE10+
