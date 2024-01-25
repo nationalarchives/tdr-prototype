6a5b02a69f23c6cf394ae467201b90d39517483c
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2024-01-25 12:52:18 +0000

    Updateing functionality of autocomplete

--- app/assets/typescript/main.ts
+++ app/assets/typescript/main.ts
@@ -71,12 +71,24 @@ window.addEventListener("DOMContentLoaded", (event) => {
         showAllValues: true,
         autoselect: false,
         confirmOnBlur: true,
-        placeholder: 'Start typing...',
+        placeholder: 'Start typing to search',
         dropdownArrow: function (config:any) {
-          // return '<svg class="' + config.className + '" style="top: 8px;" viewBox="0 0 512 512" ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>'
-          return ''
+          return '<svg class="' + config.className + '" style="top: 8px;" viewBox="0 0 512 512" ><path d="M256,298.3L256,298.3L256,298.3l174.2-167.2c4.3-4.2,11.4-4.1,15.8,0.2l30.6,29.9c4.4,4.3,4.5,11.3,0.2,15.5L264.1,380.9  c-2.2,2.2-5.2,3.2-8.1,3c-3,0.1-5.9-0.9-8.1-3L35.2,176.7c-4.3-4.2-4.2-11.2,0.2-15.5L66,131.3c4.4-4.3,11.5-4.4,15.8-0.2L256,298.3  z"/></svg>'
+          // return ''
         }
       })
+
+      // Problem: It is potentially confusing if youn click into the element and it has the previous
+      // folder still present. It isn't clear that when you do this you need to remove the
+      // current input value to shows all the results.
+      // Action: The listener below means that whenever the filter is clicked it clears the
+      // current search so the full list is shown again.
+      const directoryInput: HTMLInputElement = document.querySelector('input#filter-by-dir');
+      directoryInput.addEventListener('focus', (e:Event) => {
+        directoryInput.value = ""
+      })
+
+
     }
 
 });
