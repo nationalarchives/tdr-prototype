0ca688c525473be4e7e07dfdf63f66a589b901c1
Gareth Foote <gareth.foote@nationalarchives.gov.uk>
2025-01-07 15:25:54 +0000

    Simplifying prototype version documentation

--- app/routes-versions.js
+++ app/routes-versions.js
@@ -2,6 +2,92 @@ const govukPrototypeKit = require("govuk-prototype-kit");
 const router = govukPrototypeKit.requests.setupRouter();
 
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+
+const rootPath = __dirname+'/views/prototype-versions/versions';
+
+// Loop through all directories and return index frontmatter only 
+function extractAllIndexFm(rootPath){
+  let allIndexFm = [];
+  const subfolders = fs.readdirSync(rootPath);
+  subfolders.forEach((subfolder) => {
+    const indexPath = path.join(rootPath, subfolder, 'index.md');
+    if (fs.statSync(indexPath).isFile()){
+      const fileContent = fs.readFileSync(indexPath, 'utf8');
+      const fm = matter(fileContent);
+      fm.data.subfolder = subfolder;
+      allIndexFm.push(fm.data)
+    }
+  });
+
+  allIndexFm = allIndexFm.sort((a, b) => parseInt(b.subfolder) - parseInt(a.subfolder));
+
+  return allIndexFm;
+} 
+
+// Go into provided directory and return index and journey (front matter and markdown)
+function extractIndexAndJourneys(versionPath) { 
+  const version = { index: {}, journeys:[] };
+  let fileContent, fm;
+
+  if (fs.statSync(versionPath).isDirectory()) {
+    // Get index
+    const indexPath = path.join(versionPath, 'index.md');
+    fileContent = fs.readFileSync(indexPath, 'utf8');
+    version.index = matter(fileContent);
+
+    // Read all files inside the subfolder, filter out index 
+    // and capture the rest.
+    const files = fs.readdirSync(versionPath);
+    files.forEach((file) => {
+      const filePath = path.join(versionPath, file);
+      if (fs.statSync(filePath).isFile() && file !== 'index.md' && path.extname(filePath) === '.md'){
+        fileContent = fs.readFileSync(filePath, 'utf8');
+        fm = matter(fileContent);
+        const extractNumber = (filename) => parseInt((filename.match(/-(\d{1,2})\.md$/) || [])[1], 10) || null;
+        fm.filenumber = extractNumber(file);
+        fm.filename = file;
+        version.journeys.push(matter(fileContent));
+      }
+    });
+  }
+
+  version.journeys = version.journeys.sort((a, b) => a.filenumber - b.filenumber);
+  
+  return version;
+}
+
+router.get("/prototype-versions/", async (req, res) => {
+  try {
+    const data = extractAllIndexFm(rootPath);
+
+    res.render(`prototype-versions/index`, {
+      data: data
+    });
+
+  } catch (error) {
+    console.error(error);
+    // res.render(`prototype-versions/error`);
+  }
+});
+
+router.get("/prototype-version/:pr_number", async (req, res) => {
+  try {
+    const folderPath = path.join(rootPath, req.params.pr_number);
+    const data = extractIndexAndJourneys(folderPath);
+
+    res.render(`prototype-versions/version`, {
+      index: data.index,
+      journeys: data.journeys
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.render(`prototype-versions/error`);
+  }
+});
 
 router.get("/prototype-versions-api/", async (req, res) => {
   try {
